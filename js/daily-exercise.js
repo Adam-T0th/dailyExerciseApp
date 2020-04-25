16439718a66187953f98da15bcf89d76a0572fe0
Adam Toth <adam.toth.n@gmail.com>
2020-04-25 13:34:13 +0100

    .

--- js/daily-exercise.js
+++ js/daily-exercise.js
@@ -1,26 +1,67 @@
 /*
 Daily exercise program; according to the day of the week it displays the corresponding exercise
 */
-$(document).ready(function(){
-  $("#").click(function(){
-  });
-});
 
 function displayVals() {
     var dayValues = $("#days").val();
 
     if(dayValues == "Monday") {
       $( "p" ).html(exercisesArray[0] + "<br>"); //display the corresponding exercise string
+      $("h2").css("color", "white");
+      $("p").css("color", "white");
+      $("body").css({
+        "background-image": "url('/img/man-exercising-leg.jpg')",
+        "background-repeat": "no-repeat",
+        "background-attachment": "fixed",
+        "background-position": "center",
+        "background-size": "cover"
+    });
     } else if(dayValues == "Tuesday") {
       $( "p" ).html(exercisesArray[1]);
+      $("h2").css("color", "white");
+      $("p").css("color", "white");
+      $("body").css({
+        "background-image": "url('/img/man-doing-abs.jpg')",
+        "background-repeat": "no-repeat",
+        "background-attachment": "fixed",
+        "background-position": "center",
+        "background-size": "cover"
+    });
     } else if(dayValues == "Wednesday") {
       $( "p" ).html(exercisesArray[2]);
+      $("h2").css("color", "white");
+      $("p").css("color", "white");
+      $("body").css({
+        "background-image": "url('/img/man-doing-arms.jpg')",
+        "background-repeat": "no-repeat",
+        "background-attachment": "fixed",
+        "background-position": "center",
+        "background-size": "cover"
+    });
     } else if(dayValues == "Thursday") {
       $( "p" ).html(exercisesArray[3]);
+      $("h2").css("color", "white");
+      $("p").css("color", "white");
+      $("body").css({
+        "background-image": "url('/img/bird-dog-exercise.jpg')",
+        "background-repeat": "no-repeat",
+        "background-attachment": "fixed",
+        "background-position": "center",
+        "background-size": "cover"
+    });
     } else if(dayValues == "Friday") {
       $( "p" ).html(exercisesArray[4]);
+      $("h2").css("color", "white");
+      $("p").css("color", "white");
+      $("body").css({
+        "background-image": "url('/img/bench-press.jpg')",
+        "background-repeat": "no-repeat",
+        "background-attachment": "fixed",
+        "background-position": "center",
+        "background-size": "cover"
+    });
     } else if(dayValues == "") {
-      $( "p" ).html("<p>What day of the week is it?</p>"); //default behavior; not working yet
+      $( "p" ).html("<p>What day of the week is it?</p>");
     }
 }
 
